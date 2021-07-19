d54152c9345f1c45f5432a31b8bda6c002360121
ZingerY <Vitaliks90@mail.ru>
2021-07-19 17:37:44 +0300

    PRO_JS lesson-3 homework

--- PROF_LAYOUT/js/main.js
+++ PROF_LAYOUT/js/main.js
@@ -1,8 +1,18 @@
 'use strict';
 
 window.addEventListener('load', () => {
-	const catalog = new Catalog(goods);
-	catalog.render();
-	const cart = new Cart(catalog);
-	cart.init();
+	fetch('catalog.json')
+	// fetch('https://zingery.ru/catalog.php')
+		.then((response) => {
+			return response.json()
+		})
+		.then((response) => {
+			const catalog = new Catalog(response);
+			catalog.render();
+			const cart = new Cart(catalog);
+			cart.init();
+		})
+		.catch((err) => {
+			console.log(err)
+		})
 });
