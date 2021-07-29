4a694a4471f5ac0781b18fe599c347c41ca34aac
ZingerY <Vitaliks90@mail.ru>
2021-07-29 18:26:14 +0300

    PRO_JS lesson-5 homework

--- PROF_LAYOUT/js/main.js
+++ PROF_LAYOUT/js/main.js
@@ -1,18 +1,39 @@
 'use strict';
 
-window.addEventListener('load', () => {
-	fetch('catalog.json')
-	// fetch('https://zingery.ru/catalog.php')
-		.then((response) => {
-			return response.json()
-		})
-		.then((response) => {
-			const catalog = new Catalog(response);
-			catalog.render();
-			const cart = new Cart(catalog);
-			cart.init();
-		})
-		.catch((err) => {
-			console.log(err)
-		})
+const vue = new Vue({
+	el: '.vue',
+	data: {
+		showSearch: false,
+		goods: [],
+		searchLine: '',
+		sumCount: 0,
+		showCart: false,
+		goodsList: [],
+	},
+	computed: {
+		searchGoods() {
+			return this.goods.filter(e => this.searchLine && e.name.includes(this.searchLine));
+		}
+	},
+	methods: {
+		visibleSearch() {
+			this.showSearch = !this.showSearch;
+		},
+
+	},
+	mounted() {
+		// fetch('catalog.json')
+		fetch('https://zingery.ru/catalog.php')
+			.then(response => response.json())
+			.then(data => {
+				this.goods = data;
+				const catalog = new Catalog(data, this);
+				catalog.render();
+				const cart = new Cart(catalog, this);
+				cart.init();
+			})
+			.catch((err) => {
+				console.log(err)
+			});
+	}
 });
