fabbe6b49bc1f748525ff1f4d5d46f458e4c5b3c
ZingerY <Vitaliks90@mail.ru>
2021-08-15 22:14:37 +0300

    REACT JS lesson 1 homework

---
+++ REACT_JS/lesson_1/src/App.js
@@ -0,0 +1,12 @@
+import {Message} from "./components/Message"
+
+function App() {
+	return (
+		<div className = "App" >
+			<Message text="Hello World!!!"/>
+			<Message text="Hello All!!!"/>
+		</div>
+	);
+}
+
+export default App;
